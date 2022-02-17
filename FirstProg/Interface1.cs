daaa8bad0eb225b65bb7148c83a771640627d5e3
Arman Tyagi <Arman.Tyagi@bhavnacorp.com>
2022-02-17 17:45:16 +0530

    Interface Practice

---
+++ FirstProg/Interface1.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+
+//namespace FirstProg
+//{
+//    interface IInterface1
+//    {
+//        void myMethod();
+//    }
+//    interface IInterface2
+//    {
+//        void myOtherMethod();
+//    }
+//    class DemoClass : IInterface1, IInterface2
+//    {
+//        public void myMethod()
+//        {
+//            Console.WriteLine("Arman tyagi");
+//        }
+//        public void myOtherMethod()
+//        {
+//            Console.WriteLine("About Arman Tyagi..........");
+//        }
+//    }
+//    class Ati
+//    {
+//        static void Main(string[]
+//        {
+//            DemoClass myObj = new DemoClass();
+//            myObj.myMethod();
+//            myObj.myOtherMethod();
+//        }
+//    }
+//}
---
+++ FirstProg/Player.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace FirstProg
+//{
+//    class Student 
+//    {
+//        public int StudentId { get; set; }
+//        public  string Name  { get; set; }
+//        public string Address { get; set; }
+//        public DateTime DOB { get; set; }
+
+
+//        //methods
+//        public void ShowDetails()
+//        {
+//            Console.WriteLine("Student Information");
+//            Console.WriteLine("StudentId : {0}, Name:{1}, Address:{2}, DOB:{3}", StudentId, Name, Address, DOB);
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Student st = new Student();
+//            st.StudentId = 1;
+//            st.Name = "Arman Tyagi";
+//            st.Address = "New Delhi";
+//            st.DOB = new DateTime(2000, 09, 27);
+
+//            st.ShowDetails();
+//            Console.ReadLine();
+//        }
+//    }
+//}
